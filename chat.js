0bcb13ddd23e09c5b2cebc52ee66a4fb6da0529d
Taylor Kimmett <taylor@pubnub.com>
2013-06-05 14:04:30 -0700

    All basic functionality is in: -Presence -Messaging

--- chat.js
+++ chat.js
@@ -1,6 +1,16 @@
-// Fill window below the top bar with the chat screen
+/***
+	-Fit chat screen vertically below top bar
+	-Fit message area horizontally left of member area
+	-Fit messages vertically above input box
+ ***/
 function onResize() {
-	$(".chat-screen").height(window.innerHeight - $(".top-bar").height());
+	$(".chat-screen").height(window.innerHeight - $(".top-bar").outerHeight());
+	var mArea = $(".message-area");
+	mArea.width(window.innerWidth - $(".member-area").outerWidth());
+	$(".messages").height(mArea.innerHeight() - $(".message-input").outerHeight() - 20);
+	var mInput = $(".message-input");
+		mInput.width(mArea.innerWidth() * 0.80);
+		mInput.css("left", mArea.innerWidth() * 0.10);
 };
 $(window).resize(onResize);
 onResize();
@@ -8,6 +18,8 @@ onResize();
 /***
 	PubNub Stuff and Chat Logic
  ***/
+
+var CHANNEL = "chat"
 
 // Initialize PubNub
 var P = PUBNUB.init({
@@ -23,10 +35,11 @@ function handlePresence(msg) {
 		$(".member-area p:contains('" + msg.uuid + "')").animate({ marginLeft: "100%" }, function () {
 			$(this).remove()
 		});
+
 		// Someone is backing out because they tried to use your name. Resubscribe.
 		if (msg.uuid == localStorage.chatName) {
 			P.subscribe({
-				channel: "chat",
+				channel: CHANNEL,
 				presence: handlePresence,
 				callback: handleMessage
 			});
@@ -40,19 +53,23 @@ function handlePresence(msg) {
 };
 
 function handleMessage(msg) {
-	console.log(JSON.stringify(msg));
+	console.log(msg);
+	var msg = JSON.parse(msg);
+	var messages = $(".messages");
+	messages.append("<div class='message'><p class='body'>" + msg.text + "</p><p class='source'>" + (msg.source == localStorage.chatName ? "You" : msg.source) + "</p></div>")
+	messages.animate({ scrollTop: messages.height() });
 };
 
-// Subscribe to presence
+// Subscribe to presence and messages
 P.subscribe({
-	channel: "chat",
+	channel: CHANNEL,
 	presence: handlePresence,
 	callback: handleMessage
 });
 
 // Get the current room members and check for taken name
 P.here_now({
-	channel: "chat",
+	channel: CHANNEL,
 	callback: function (msg) {
 		console.log("Here_now: " + JSON.stringify(msg));
 		var uuids = msg.uuids;
@@ -60,8 +77,9 @@ P.here_now({
 			var uuid = uuids[i];
 			if (uuid == localStorage.chatName) {
 				P.unsubscribe({ channel: "chat" });
+				alert("The name '" + localStorage.chatName + "' is already in use. Please choose a different one.");
 				delete localStorage.chatName;
-				alert("Your name is already in use. Please choose a different one.");
+
 				// Go back to name selection
 				$(".main-area").load("./register.html");
 				return;
@@ -76,5 +94,32 @@ P.here_now({
 		var uBox = $(".user-box");
 		uBox[0].innerHTML = localStorage.chatName;
 		uBox.animate({ marginRight: "0px" });
+	}
+});
+
+/***
+	Pressing enter in the input sends a message
+***/
+$(".message-input").keyup(function (e) {
+	console.log(e);
+	if (e.keyCode == 13) {
+		var input = $(this);
+		var txt = input.val();
+		input.val("");
+		P.publish({
+			channel: CHANNEL,
+			message: JSON.stringify({
+				text: txt,
+				source: localStorage.chatName,
+				time: Date.now()
+			}),
+			callback: function(res) {
+				// If the message couldn't be sent, replace the text in the input and show an error message
+				if (!res[0]) {
+					alert("Unable to send message: " + res[2]);
+					input.val(txt);
+				}
+			}
+		});
 	}
 });
